f6a3aedca575095fce2b63ab15840ead62bc3380
Andreas Olsson <olsson.andreas@gmail.com>
2019-08-01 17:03:43 +0200

    New results file Refactored into one big method and a main-method

--- app/app.py
+++ app/app.py
@@ -33,67 +33,72 @@ def print_to_file(resultsList, classname, filepath, filepathtemplate):
 def copy_template_images_to_output(imagesFolderPath, outputPath):
     copy_tree(imagesFolderPath, os.path.join(outputPath, 'images/'))
 
-gtefilename = 'GTETable.html'
-hpdfilename = 'HPDTable.html'
-lmp1filename = 'LMP1Table.html'
 
-gteimagename = 'GTE.jpg'
-hpdimagename = 'HPD.jpg'
-lmp1imagename = 'LMP1.jpg'
+def generate_results():
+    gtefilename = 'GTETable.html'
+    hpdfilename = 'HPDTable.html'
+    lmp1filename = 'LMP1Table.html'
 
-here = os.path.dirname(os.path.abspath(__file__))
+    gteimagename = 'GTE.jpg'
+    hpdimagename = 'HPD.jpg'
+    lmp1imagename = 'LMP1.jpg'
 
-resultsfilepath = os.path.join(here, 'results.csv') # results file should be in "root"
-templatefilepath = os.path.join(here, 'template.html') # template file should be in "root"
+    here = os.path.dirname(os.path.abspath(__file__))
 
-output = os.path.join(here, 'output/') # All output in the output folder
+    resultsfilepath = os.path.join(here, 'results.csv') # results file should be in "root"
+    templatefilepath = os.path.join(here, 'template.html') # template file should be in "root"
 
-print("Output:", output)
+    output = os.path.join(here, 'output/') # All output in the output folder
 
-gtefilepath = os.path.join(output, gtefilename)
-if os.path.isfile(gtefilepath):
-    os.remove(gtefilepath)
+    print("Output:", output)
 
-hpdfilepath = os.path.join(output, hpdfilename)
-if os.path.isfile(hpdfilepath):
-    os.remove(hpdfilepath)
+    gtefilepath = os.path.join(output, gtefilename)
+    if os.path.isfile(gtefilepath):
+        os.remove(gtefilepath)
 
-lmp1filepath = os.path.join(output, lmp1filename)
-if os.path.isfile(lmp1filepath):
-    os.remove(lmp1filepath)
+    hpdfilepath = os.path.join(output, hpdfilename)
+    if os.path.isfile(hpdfilepath):
+        os.remove(hpdfilepath)
 
-print("Lmp1filepath:", lmp1filepath)
-with open(resultsfilepath) as csvfile:
-    #Skip first three lines (for regular race, TODO league races)
-    csvfile.readline()
-    csvfile.readline()
-    csvfile.readline()
+    lmp1filepath = os.path.join(output, lmp1filename)
+    if os.path.isfile(lmp1filepath):
+        os.remove(lmp1filepath)
 
-    results = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+    print("Lmp1filepath:", lmp1filepath)
+    with open(resultsfilepath) as csvfile:
+        #Skip first three lines (for regular race, TODO league races)
+        csvfile.readline()
+        csvfile.readline()
+        csvfile.readline()
 
-    lmp1List = []
-    hpdList = []
-    gteList = []
-    for row in results:
-        if row[ct.COL_CAR_CLASS_ID] == ct.CLASS_ID_LMP1:
-            lmp1List.append(row)
-        elif row[ct.COL_CAR_CLASS_ID] == ct.CLASS_ID_HPD:
-            hpdList.append(row)
-        elif row[ct.COL_CAR_CLASS_ID] == ct.CLASS_ID_GTE:
-            gteList.append(row)
+        results = csv.DictReader(csvfile, delimiter=',', quotechar='"')
 
-    print("LMP1 list count: ", len(lmp1List))
-    print("HPD list count: ", len(hpdList))
-    print("GTE list count: ", len(gteList))
+        lmp1List = []
+        hpdList = []
+        gteList = []
+        for row in results:
+            if row[ct.COL_CAR_CLASS_ID] == ct.CLASS_ID_LMP1:
+                lmp1List.append(row)
+            elif row[ct.COL_CAR_CLASS_ID] == ct.CLASS_ID_HPD:
+                hpdList.append(row)
+            elif row[ct.COL_CAR_CLASS_ID] == ct.CLASS_ID_GTE:
+                gteList.append(row)
 
-    copy_template_images_to_output(os.path.join(here, 'images/'), output)
+        print("LMP1 list count: ", len(lmp1List))
+        print("HPD list count: ", len(hpdList))
+        print("GTE list count: ", len(gteList))
 
-    print_to_file(lmp1List, "LMP1", lmp1filepath, templatefilepath)
+        copy_template_images_to_output(os.path.join(here, 'images/'), output)
 
-    print_to_file(hpdList, "HPD", hpdfilepath, templatefilepath)
+        print_to_file(lmp1List, "LMP1", lmp1filepath, templatefilepath)
 
-    print_to_file(gteList, "GTE", gtefilepath, templatefilepath)
+        print_to_file(hpdList, "HPD", hpdfilepath, templatefilepath)
 
-    imgkit.from_file(lmp1filepath, os.path.join(output, lmp1imagename))
-    imgkit.from_file(hpdfilepath, os.path.join(output, hpdimagename))
-    imgkit.from_file(gtefilepath, os.path.join(output, gteimagename))
+        print_to_file(gteList, "GTE", gtefilepath, templatefilepath)
+
+        imgkit.from_file(lmp1filepath, os.path.join(output, lmp1imagename))
+        imgkit.from_file(hpdfilepath, os.path.join(output, hpdimagename))
+        imgkit.from_file(gtefilepath, os.path.join(output, gteimagename))
+
+if __name__ == '__main__':
+    generate_results()
